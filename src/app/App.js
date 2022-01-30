bc37b8c1c725750fa0a806b9637e2b026f175489
Musmen <musmen@tut.by>
2022-01-30 15:07:03 +0300

    init: init base project structure with environment

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './scss/styles.scss';
+
+import React from 'react';
+
+function App() {
+  return (
+    <h1>
+      Hello, <span className="green">World!</span>
+    </h1>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 9000,
+    open: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
