af56b829bb11fbc6c5b74cff086e00fc302610ae
Musmen <musmen@tut.by>
2022-02-03 22:31:30 +0300

    feat: add temperature switcher

---
+++ src/app/components/temperature-switcher/TemperatureSwitcher.js
@@ -0,0 +1,35 @@
+import './temperature-switcher.styles.scss';
+
+import React from 'react';
+import propTypes from 'prop-types';
+
+function TemperatureSwitcher({ isCelcius, changeTemperatureUnit }) {
+  return (
+    <div className="switcher__container">
+      <label className="switcher__label" htmlFor="switcher">
+        <input
+          className="switcher"
+          type="checkbox"
+          name="switcher"
+          id="switcher"
+          onChange={changeTemperatureUnit}
+          checked={isCelcius}
+        />
+        <span className="switcher__inner" />
+        <span className="switcher__switch" />
+      </label>
+    </div>
+  );
+}
+
+TemperatureSwitcher.propTypes = {
+  isCelcius: propTypes.bool,
+  changeTemperatureUnit: propTypes.func,
+};
+
+TemperatureSwitcher.defaultProps = {
+  isCelcius: true,
+  changeTemperatureUnit: () => {},
+};
+
+export default TemperatureSwitcher;
--- src/app/containers/control-block/ControlBlock.js
+++ src/app/containers/control-block/ControlBlock.js
@@ -1,8 +1,9 @@
 import './control-block.styles.scss';
 
-import React from 'react';
+import React, { useState } from 'react';
 import BgChangeButton from '../../components/bg-change-button/bg-change-button';
 import fetchImage from '../../api/flickr';
+import TemperatureSwitcher from '../../components/temperature-switcher/TemperatureSwitcher';
 
 const changeBg = async () => {
   const imageSrc = await fetchImage('winter');
@@ -10,9 +11,16 @@ const changeBg = async () => {
 };
 
 function ControlBlock() {
+  const [isCelcius, changeTemperatureUnit] = useState(true);
+
   return (
     <div className="control-block">
       <BgChangeButton changeBg={changeBg} />
+      <TemperatureSwitcher
+        isCelcius={isCelcius}
+        changeTemperatureUnit={() => changeTemperatureUnit(!isCelcius)}
+      />
+      <p>{isCelcius.toString()}</p>
     </div>
   );
 }
