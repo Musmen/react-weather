6c9f6d1dbcf914323dfd0ecf40532d3c24107e15
Musmen <musmen@internet.ru>
2022-02-19 14:33:18 +0300

    feat: add header

--- src/app/App.js
+++ src/app/App.js
@@ -5,12 +5,12 @@ import React, { useMemo, useState } from 'react';
 import ControlBlock from './containers/control-block/ControlBlock';
 import MainBlock from './containers/main-block/MainBlock';
 import LanguageContext from './components/language-context/LanguageContext';
+import Header from './components/header/Header';
 
 import { DEFAULT_LANGUAGE } from './common/constants';
 
 function App() {
   const [currentLanguage, setLanguage] = useState(DEFAULT_LANGUAGE);
-  // нужно перенести все, что касается языка в его котекст провайдер
   const changeLanguage = ({ target: { value } }) => {
     setLanguage(value);
   };
@@ -28,6 +28,7 @@ function App() {
 
   return (
     <LanguageContext.Provider value={languageState}>
+      <Header />
       <ControlBlock searchQuery={searchQuery} changeSearchQuery={changeSearchQuery} />
       <MainBlock searchQuery={searchQuery} />
     </LanguageContext.Provider>
---
+++ src/app/components/header/Header.js
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="logo">React Weather</h1>
+    </header>
+  );
+}
+
+export default Header;
